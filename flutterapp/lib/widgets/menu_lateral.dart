bea1ae9f854385a9f43e431c6ddb66dc88098ee9
Jesus Valdivia <jesusjesusvaldivia@hotmail.com>
2022-06-06 21:36:37 -0500

    mejoras

--- flutterapp/lib/widgets/menu_lateral.dart
+++ flutterapp/lib/widgets/menu_lateral.dart
@@ -6,83 +6,104 @@ class MenuLateral extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Drawer(
-      backgroundColor: Color.fromARGB(255, 22, 21, 21),
-      child: ListView(
-        padding: EdgeInsets.zero,
-        children: <Widget>[
-          UserAccountsDrawerHeader(
-            accountName: Text('Usuario'), 
-            accountEmail: Text('cuenta'),
-            currentAccountPicture: CircleAvatar(
-              child: ClipOval(
-                child: Image.network('https://st.depositphotos.com/1814084/1639/i/600/depositphotos_16398251-stock-photo-brad-pitt.jpg',
-                width: 72,
-                height: 72,
-                fit: BoxFit.cover,),
+      //backgroundColor: Color.fromARGB(255, 22, 21, 21),
+      child: Container(
+        decoration: BoxDecoration(
+          gradient: LinearGradient(
+            colors: [
+              Color.fromARGB(255, 27, 27, 27),
+              Color.fromARGB(255, 5, 5, 5),
+              Color.fromARGB(255, 2, 2, 2)
+            ],
+            begin: Alignment.bottomCenter,
+            end: Alignment.topCenter,
+          )
+        ),
+        child: ListView(
+          padding: EdgeInsets.zero,
+          children: <Widget>[
+            UserAccountsDrawerHeader(
+              accountName: Text('Usuario Creado'), 
+              accountEmail: Text('cuenta Hotmail',
+              style: TextStyle(
+                color: Colors.white,
+                fontWeight: FontWeight.bold),
+                ),
+              currentAccountPicture: CircleAvatar(
+                child: ClipOval(
+                  child: Image.network('https://st.depositphotos.com/1814084/1639/i/600/depositphotos_16398251-stock-photo-brad-pitt.jpg',
+                  width: 72,
+                  height: 72,
+                  fit: BoxFit.cover,),
+                ),
               ),
-            ),
-            decoration: BoxDecoration(
-              color: Colors.orange,
-              image: DecorationImage(
-                image: NetworkImage('https://www.dondeir.com/wp-content/uploads/2021/06/paginas-para-descargar-libros-gratis-y-legales-portada-1024x767.jpg'),
-                fit: BoxFit.cover
-              )
-            ),
-            ),
-          
-          
-          ListTile(
-            leading: Icon(Icons.home,color: Colors.grey),
-            title: Text('Inicio',
-            style: TextStyle(
-              color: Colors.white,
-              fontSize: 15,
+              decoration: BoxDecoration(
+                color: Color.fromARGB(255, 22, 21, 21),
+                borderRadius: BorderRadius.only(
+                  bottomLeft: Radius.circular(15),
+                  bottomRight: Radius.circular(15),
+                ),
+                image: DecorationImage(
+                  image: NetworkImage('https://www.dondeir.com/wp-content/uploads/2021/06/paginas-para-descargar-libros-gratis-y-legales-portada-1024x767.jpg'),
+                  fit: BoxFit.cover
+                )
               ),
+              ),
+            
+            
+            ListTile(
+              leading: Icon(Icons.home,color: Colors.grey),
+              title: Text('Inicio',
+              style: TextStyle(
+                color: Colors.white,
+                fontSize: 15,
+                ),
+              ),
+              onTap: (){
+                Navigator.pushNamed(context, 'principal_screen');
+              },
             ),
-            onTap: (){
-              Navigator.pushNamed(context, 'principal_screen');
-            },
-          ),
 
-          ListTile(
-            leading: Icon(Icons.book_sharp,color: Colors.grey),
-            title: Text('Categorias',
-            style: TextStyle(
-              color: Colors.white,
-              fontSize: 15,
+            ListTile(
+              leading: Icon(Icons.book_sharp,color: Colors.grey),
+              title: Text('Categorias',
+              style: TextStyle(
+                color: Colors.white,
+                fontSize: 15,
+                ),
               ),
+              onTap: (){
+                Navigator.pushNamed(context, 'categorias_screen');
+              },
             ),
-            onTap: (){
-              Navigator.pushNamed(context, 'categorias_screen');
-            },
-          ),
 
-          ListTile(
-            leading: Icon(Icons.supervised_user_circle,color: Colors.grey),
-            title: Text('Perfil',
-            style: TextStyle(
-              color: Colors.white,
-              fontSize: 15,
+            ListTile(
+              leading: Icon(Icons.supervised_user_circle,color: Colors.grey),
+              title: Text('Perfil',
+              style: TextStyle(
+                color: Colors.white,
+                fontSize: 15,
+                ),
               ),
+              onTap: (){
+                Navigator.pushNamed(context, 'principal_screen');
+              },
             ),
-            onTap: (){
-              Navigator.pushNamed(context, 'principal_screen');
-            },
-          ),
 
-          ListTile(
-            leading: Icon(Icons.exit_to_app,color: Colors.grey),
-            title: Text('Cerrar Sesion',
-            style: TextStyle(
-              color: Colors.white,
-              fontSize: 15,
+            ListTile(
+              leading: Icon(Icons.exit_to_app,color: Colors.grey),
+              title: Text('Cerrar Sesion',
+              style: TextStyle(
+                color: Colors.white,
+                fontSize: 15,
+                ),
               ),
+              onTap: (){
+                Navigator.pushNamed(context, '/');
+              },
             ),
-            onTap: (){
-              Navigator.pushNamed(context, '/');
-            },
-          ),
-        ],
+          ],
+        ),
       ),
     );
   }
